009597175cfca4468ee843e9c623b14f9acfcf44
Trevor Gravely <graveltr@umich.edu>
2023-06-26 21:18:46 -0500

    Basic entry parsing implemented, basic test 1 passed.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 
 pub struct Config {
     // file path of Clippings.txt
     pub file_path: String,
-    // path to booknotes directory 
+    // path to booknotes directory
     pub booknotes_dir: String,
     // flags
     pub fullsync: bool,
-
 }
 
 impl Config {
@@ -20,63 +19,90 @@ impl Config {
         let file_path = args[1].clone();
         let booknotes_dir = args[2].clone();
         let fullsync = true;
-        Ok(Config {file_path, booknotes_dir, fullsync})
+        Ok(Config {
+            file_path,
+            booknotes_dir,
+            fullsync,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    /* 
-     * The clippings format from the kindle is 
+    /*
+     * The clippings format from the kindle is
      *     Book Title (Author)
-     *     - line denoting location and date 
-     *     empty line 
-     *     Quote all on one line 
+     *     - line denoting location and date
+     *     empty line
+     *     Quote all on one line
      *     ========
      *     <feff>Book Title (Author)
-     *     - line denoting location and date 
-     *     empty line 
+     *     - line denoting location and date
+     *     empty line
      *     .
      *     .
      *     .
      *     ========
      *     <feff>Book Title (Author)
-     *     - line denoting location and date 
-     *     empty line 
+     *     - line denoting location and date
+     *     empty line
      *
-     *     Quote all on one line 
-     *     ======== EOF 
+     *     Quote all on one line
+     *     ======== EOF
      *
-     * (1) Create Entry {Title, Author, Quote} by reading 4 lines and discarding 
-     * junk. Pass the Entry object to a function. 
-     * (2) This function searches obsidian book-notes folder for a file of the name 
-     * Book title by author. If no such file is found, create a new file and add the 
+     * (1) Create Entry {Title, Author, Quote} by reading 4 lines and discarding
+     * junk. Pass the Entry object to a function.
+     * (2) This function searches obsidian book-notes folder for a file of the name
+     * Book title by author. If no such file is found, create a new file and add the
      * entry to it.
      * */
-
 
     Ok(())
 }
 
 #[derive(PartialEq, Debug)]
 struct Entry {
-    title: String, 
+    title: String,
     author: String,
     location: String,
     quote: String,
 }
 
+/*
+ * Expects: Reference to a vector of Strings containing lines to be parsed.
+ * This vector should contain 3 lines.
+ * Returns: Returns an Entry object containing the parsed information.
+ * TODO: fix error handling, don't return entry, return result
+ */
 fn parse_delimited_lines(lines: &[String]) -> Entry {
-    Entry { 
-        title: String::from("a"), 
-        author: String::from("b"), 
-        location: String::from("c"), 
-        quote: String::from("d") 
+    assert_eq!(lines.len(), 3);
+
+    /* line 1 processing */
+
+    // line 1 contains the title and author
+    let mut v: Vec<&str> = lines[0].split('(').collect();
+    let title = v[0].strip_suffix(' ').unwrap();
+    let author = v[1].strip_suffix(')').unwrap();
+
+    /* line 2 processing */
+
+    // tokenize the string into words
+    v = lines[1].split(' ').collect();
+    // find the entry after "Location" entry
+    let index_of_location_keyword = v.iter().position(|&word| word == "Location").unwrap();
+    let location = v[index_of_location_keyword + 1];
+
+    /* line 3 processing */
+    let quote = lines[2].clone();
+
+    Entry {
+        title: title.to_string(),
+        author: author.to_string(),
+        location: location.to_string(),
+        quote: quote,
     }
 }
 
-fn insert_entry(config: &Config) {
-
-}
+fn insert_entry(config: &Config) {}
 
 // TESTING //
 
@@ -88,15 +114,17 @@ mod tests {
     fn test_parse_delimited_lines() {
         let mut input: Vec<String> = Vec::new();
         input.push(String::from("Pilgrim at Tinker Creek (Annie Dillard)"));
-        input.push(String::from("- Your Highlight on Location 53-53 |\
-                   Added on Tuesday, June 20, 2023 7:58:48 PM"));
+        input.push(String::from(
+            "- Your Highlight on Location 53-53 |\
+                   Added on Tuesday, June 20, 2023 7:58:48 PM",
+        ));
         input.push(String::from("finally"));
 
-        let expected = Entry { 
-            title: String::from("Pilgrim at Tinker Creek"), 
-            author: String::from("Annie Dillard"), 
-            location: String::from("53-53"), 
-            quote: String::from("finally") 
+        let expected = Entry {
+            title: String::from("Pilgrim at Tinker Creek"),
+            author: String::from("Annie Dillard"),
+            location: String::from("53-53"),
+            quote: String::from("finally"),
         };
         assert_eq!(expected, parse_delimited_lines(&input));
     }
